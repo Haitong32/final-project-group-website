35e905271e1d4a5f33d9908817f1b5e74401540d
josephcastanon <joseph@Josephs-MBP.wireless.newschool.edu>
2016-12-16 01:25:35 -0500

    new homepage updates :)

--- scripts.js
+++ scripts.js
@@ -1,1 +1,80 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $(function() {
+
+
+  	// Fade in images
+
+  	$('img').load(function() {
+          	$(this).fadeIn('slow');
+      		});
+
+
+  	// Made draggables draggable
+
+  	$( ".draggable" ).draggable({ containment: ".frame", cursor: "crosshair",  stack: ".draggable" });
+
+
+  	// Checkboxes switch function -- changes classes
+
+  	$("input[type='checkbox']").change(function() {
+
+  			switch (this.id) {
+
+  				case 'fluffy':
+  					if($(this).is(':checked')){
+  							$("body").addClass("fluffy");
+  	        			} else {
+  	        				$("body").removeClass("fluffy");
+  	        				}
+  	    			break;
+
+
+
+  				case 'email':
+  					if($(this).is(':checked')){
+  							$(".email").css("visibility","visible");
+  	        			} else {
+  	        				$(".email").css("visibility","hidden");
+  	        				}
+  	    			break;
+
+
+
+  				case 'bio':
+  					if($(this).is(':checked')){
+  							$(".bio").css("visibility","visible");
+  	        			} else {
+  	        				$(".bio").css("visibility","hidden");
+  	        				}
+  	    			break;
+
+
+
+  				case 'work':
+  					if($(this).is(':checked')){
+  							$(".work").css("visibility","visible");
+  	        			} else {
+  	        				$(".work").css("visibility","hidden");
+  	        				}
+  	    			break;
+  	    			};
+
+
+
+  	    		});
+
+  	$(function() {
+  	    	$('img[data-hover]').hover(function() {
+  	        	$(this)
+  	            	.attr('tmp', $(this).attr('src'))
+  	            	.attr('src', $(this).attr('data-hover'))
+  	            	.attr('data-hover', $(this).attr('tmp'))
+  	            	.removeAttr('tmp');
+  	    		}).each(function() {
+  	        $('<img />').attr('src', $(this).attr('data-hover'));
+  	    		});;
+  			});
+
+
+  });
+});
